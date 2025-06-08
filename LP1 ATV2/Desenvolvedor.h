f642842e2d3e5c95974cf74035f636f1a0c12bec
luanmoon <luanmoon2005@gmail.com>
2025-06-08 17:13:08 -0300

    Update Desenvolvedor.h

--- LP1 ATV2/Desenvolvedor.h
+++ LP1 ATV2/Desenvolvedor.h
@@ -1,1 +1,17 @@
+#ifndef DESENVOLVEDOR_H
+#define DESENVOLVEDOR_H
 
+#include "Funcionario.h" 
+class Desenvolvedor : public Funcionario {
+private:
+    int quantidadeDeProjetos;
+
+public:
+    void setQuantidadeDeProjetos(int q);
+    int getQuantidadeDeProjetos() const;
+
+    float calcularSalarioFinal() override;
+    void exibirInformacoes() override;
+};
+
+#endif 
